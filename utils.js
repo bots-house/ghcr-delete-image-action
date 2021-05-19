db66f7dbeab5ef4f72e829a098734328a1f05600
Sasha Savchuk <sasha@bots.house>
2021-05-19 15:59:31 +0300

    feat(utils): add findPackageVersionByTag method

--- utils.js
+++ utils.js
@@ -9,6 +9,36 @@ let getConfig = function () {
   };
 };
 
+let findPackageVersionByTag = async function (octokit, owner, name, tag) {
+  const tags = new Set();
+
+  for await (const response of octokit.paginate.iterator(
+    octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg,
+    {
+      package_type: "container",
+      package_name: name,
+      org: owner,
+      state: "active",
+      per_page: 100,
+    }
+  )) {
+    for (let packageVersion of response.data) {
+      const versionTags = packageVersion.metadata.container.tags;
+      
+      if(versionTags.includes(tag)) {
+        return packageVersion
+      } else {
+        versionTags.map(item => {
+          tags.add(item)
+        })
+      }
+    }
+  }
+
+  throw new Error(`package with tag '${tag}' does not exits, available tags: ${Array.from(tags).join(', ')}`)
+};
+
 module.exports = {
   getConfig,
+  findPackageVersionByTag,
 };
--- utils.test.js
+++ utils.test.js
@@ -1,26 +1,52 @@
-const utils = require('./utils')
+const utils = require("./utils");
+const github = require("@actions/github");
 
-describe('getConfig', () => {
-    
-    test('throw error if value is missing', () => {
-        expect(() => {
-            utils.getConfig()
-        }).toThrow()
-    })
-    
-    test('returns ', () => {
-        process.env['INPUT_OWNER'] = 'bots-house'
-        process.env['INPUT_NAME'] = 'ghcr-delete-image'
-        process.env['INPUT_TOKEN'] = 'some-token'
-        process.env['INPUT_TAG'] = 'latest'
-    
-        expect(utils.getConfig()).toStrictEqual({
-            owner: 'bots-house',
-            name: 'ghcr-delete-image',
-            token: 'some-token',
-            tag: 'latest'
-        })
-    })
-})
+describe("getConfig", () => {
+  test("throw error if value is missing", () => {
+    expect(() => {
+      utils.getConfig();
+    }).toThrow();
+  });
 
+  test("returns valid config", () => {
+    process.env["INPUT_OWNER"] = "bots-house";
+    process.env["INPUT_NAME"] = "ghcr-delete-image";
+    process.env["INPUT_TOKEN"] = "some-token";
+    process.env["INPUT_TAG"] = "latest";
 
+    expect(utils.getConfig()).toStrictEqual({
+      owner: "bots-house",
+      name: "ghcr-delete-image",
+      token: "some-token",
+      tag: "latest",
+    });
+  });
+});
+
+describe("findPackageVersionByTag", () => {
+  const token = process.env["TEST_TOKEN"];
+  expect(token).toBeTruthy();
+
+  const octokit = github.getOctokit(token);
+
+  test("existing version returns object", async () => {
+    const packageVersion = await utils.findPackageVersionByTag(
+      octokit,
+      "bots-house",
+      "docker-telegram-bot-api",
+      "fbb8b4c-b21d667"
+    );
+    expect(packageVersion.id).toBe(266441);
+  }, 15000);
+
+  test("not existing version throw error", () => {
+
+    return expect(utils.findPackageVersionByTag(
+      octokit,
+      "bots-house",
+      "docker-telegram-bot-api",
+      "test"
+    )).rejects.toThrow(new RegExp("package with tag"));
+
+  });
+});
