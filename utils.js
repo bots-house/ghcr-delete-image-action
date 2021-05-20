8ad9844765a47a28ee289f054323f56ebe8fd3e9
Sasha Savchuk <sasha@bots.house>
2021-05-20 21:35:43 +0300

    feat: delete untagged (keep latest)  (#7)
    * chore: update action metadata
    
    * feat(utils): add new options to config
    
    * add integration test
    
    * split index.js
    
    part of #6

--- utils.js
+++ utils.js
@@ -1,17 +1,98 @@
 const core = require("@actions/core");
 
+/**
+ * Parse input from env.
+ * @returns Config
+ */
 let getConfig = function () {
-  return {
+  const config = {
     owner: core.getInput("owner", { required: true }),
     name: core.getInput("name", { required: true }),
     token: core.getInput("token", { required: true }),
-    tag: core.getInput("tag", { required: true }),
+
+    // optional, mutual exclusive options
+    tag: core.getInput("tag") || null,
+    untaggedKeepLatest: core.getInput("untagged-keep-latest") || null,
+    untaggedOlderThan: core.getInput("untagged-older-than") || null,
   };
+
+  const definedOptionsCount = [
+    config.tag,
+    config.untaggedKeepLatest,
+    config.untaggedOlderThan,
+  ].filter((x) => x !== null).length;
+
+  if (definedOptionsCount == 0) {
+    throw new Error("no any required options defined");
+  } else if (definedOptionsCount > 1) {
+    throw new Error("too many selectors defined, use only one");
+  }
+
+  if (config.untaggedKeepLatest) {
+    if (
+      isNaN((config.untaggedKeepLatest = parseInt(config.untaggedKeepLatest)))
+    ) {
+      throw new Error("untagged-keep-latest is not number");
+    }
+  }
+
+  if (config.untaggedOlderThan) {
+    if (
+      isNaN((config.untaggedOlderThan = parseInt(config.untaggedOlderThan)))
+    ) {
+      throw new Error("untagged-older-than is not number");
+    }
+  }
+
+  return config;
 };
 
 let findPackageVersionByTag = async function (octokit, owner, name, tag) {
   const tags = new Set();
 
+  for await (const pkgVer of iteratePackageVersions(octokit, owner, name)) {
+    const versionTags = pkgVer.metadata.container.tags;
+
+    if (versionTags.includes(tag)) {
+      return pkgVer;
+    } else {
+      versionTags.map((item) => {
+        tags.add(item);
+      });
+    }
+  }
+
+  throw new Error(
+    `package with tag '${tag}' does not exits, available tags: ${Array.from(
+      tags
+    ).join(", ")}`
+  );
+};
+
+let findPackageVersionsUntaggedOrderGreaterThan = async function (
+  octokit,
+  owner,
+  name,
+  n
+) {
+  const pkgs = [];
+
+  for await (const pkgVer of iteratePackageVersions(octokit, owner, name)) {
+    const versionTags = pkgVer.metadata.container.tags;
+    core.info(JSON.stringify(pkgVer), null, 2);
+    if (versionTags.length == 0) {
+      pkgs.push(pkgVer);
+    }
+  }
+
+  pkgs.sort((a, b) => {
+    return new Date(b.updated_at) - new Date(a.updated_at);
+  });
+
+  return pkgs.slice(n);
+};
+
+let iteratePackageVersions = async function* (octokit, owner, name) {
   for await (const response of octokit.paginate.iterator(
     octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg,
     {
@@ -23,23 +104,9 @@ let findPackageVersionByTag = async function (octokit, owner, name, tag) {
     }
   )) {
     for (let packageVersion of response.data) {
-      const versionTags = packageVersion.metadata.container.tags;
-
-      if (versionTags.includes(tag)) {
-        return packageVersion;
-      } else {
-        versionTags.map((item) => {
-          tags.add(item);
-        });
-      }
+      yield packageVersion;
     }
   }
-
-  throw new Error(
-    `package with tag '${tag}' does not exits, available tags: ${Array.from(
-      tags
-    ).join(", ")}`
-  );
 };
 
 let deletePackageVersion = async (octokit, owner, name, versionId) => {
@@ -51,8 +118,16 @@ let deletePackageVersion = async (octokit, owner, name, versionId) => {
   });
 };
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 module.exports = {
   getConfig,
   findPackageVersionByTag,
   deletePackageVersion,
+  findPackageVersionsUntaggedOrderGreaterThan,
+  sleep,
 };
--- utils.test.js
+++ utils.test.js
@@ -1,6 +1,18 @@
 const utils = require("./utils");
 const github = require("@actions/github");
 
+let withEnv = (envs, cb) => {
+  for (const k in envs) {
+    process.env[k] = envs[k];
+  }
+
+  cb();
+
+  for (const k in envs) {
+    delete process.env[k];
+  }
+};
+
 describe("getConfig", () => {
   test("throw error if value is missing", () => {
     expect(() => {
@@ -8,18 +20,85 @@ describe("getConfig", () => {
     }).toThrow();
   });
 
-  test("returns valid config", () => {
-    process.env["INPUT_OWNER"] = "bots-house";
-    process.env["INPUT_NAME"] = "ghcr-delete-image-action";
-    process.env["INPUT_TOKEN"] = "some-token";
-    process.env["INPUT_TAG"] = "latest";
+  const sharedRequiredOpts = {
+    INPUT_OWNER: "bots-house",
+    INPUT_NAME: "ghcr-delete-image-action",
+    INPUT_TOKEN: "some-token",
+  };
 
-    expect(utils.getConfig()).toStrictEqual({
-      owner: "bots-house",
-      name: "ghcr-delete-image-action",
-      token: "some-token",
-      tag: "latest",
-    });
+  test("returns valid config", () => {
+    withEnv(
+      {
+        ...sharedRequiredOpts,
+        INPUT_TAG: "latest",
+      },
+      () => {
+        expect(utils.getConfig()).toStrictEqual({
+          owner: "bots-house",
+          name: "ghcr-delete-image-action",
+          token: "some-token",
+          tag: "latest",
+          untaggedKeepLatest: null,
+          untaggedOlderThan: null,
+        });
+      }
+    );
+  });
+
+  test("throw error if no any required defined", () => {
+    withEnv(
+      {
+        ...sharedRequiredOpts,
+      },
+      () => {
+        expect(() => utils.getConfig()).toThrow(
+          "no any required options defined"
+        );
+      }
+    );
+  });
+
+  test("throw error if more then on selector defined", () => {
+    withEnv(
+      {
+        ...sharedRequiredOpts,
+        "INPUT_UNTAGGED-KEEP-LATEST": "2",
+        "INPUT_UNTAGGED-OLDER-THAN": "3",
+      },
+      () => {
+        expect(() => utils.getConfig()).toThrow(
+          "too many selectors defined, use only one"
+        );
+      }
+    );
+  });
+
+  test("throw error if untagged keep latest is not number", () => {
+    withEnv(
+      {
+        ...sharedRequiredOpts,
+        "INPUT_UNTAGGED-KEEP-LATEST": "asdf",
+      },
+      () => {
+        expect(() => utils.getConfig()).toThrow(
+          "untagged-keep-latest is not number"
+        );
+      }
+    );
+  });
+
+  test("throw error if untagged older than is not number", () => {
+    withEnv(
+      {
+        ...sharedRequiredOpts,
+        "INPUT_UNTAGGED-OLDER-THAN": "asdf",
+      },
+      () => {
+        expect(() => utils.getConfig()).toThrow(
+          "untagged-older-than is not number"
+        );
+      }
+    );
   });
 });
 
@@ -49,4 +128,34 @@ describe("findPackageVersionByTag", () => {
       )
     ).rejects.toThrow(new RegExp("package with tag"));
   });
+});
+
+describe("findPackageVersionsUntaggedOrderGreaterThan", () => {
+  const token = process.env["INTEGRATION_TEST_TOKEN"];
+  expect(token).toBeTruthy();
+
+  const octokit = github.getOctokit(token);
+
+  test("returns greater than 5 objects", async () => {
+    const pkgs = await utils.findPackageVersionsUntaggedOrderGreaterThan(
+      octokit,
+      "bots-house",
+      "docker-telegram-bot-api",
+      2
+    );
+
+    expect(pkgs.length).toBeGreaterThanOrEqual(5);
+    // expect(packageVersion.id).toBe(266441);
+  }, 15000);
+
+  // test("not existing version throw error", () => {
+  //   return expect(
+  //     utils.findPackageVersionByTag(
+  //       octokit,
+  //       "bots-house",
+  //       "docker-telegram-bot-api",
+  //       "test"
+  //     )
+  //   ).rejects.toThrow(new RegExp("package with tag"));
+  // });
 });
